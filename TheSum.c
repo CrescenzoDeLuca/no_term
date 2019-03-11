4f72dacb311efb372464adbb873b54bfbeb6bc84
CrescenzoDeLuca <lmcdfa@gmail.com>
2019-03-11 12:33:33 +0100

    fatt

--- TheSum.c
+++ TheSum.c
@@ -9,8 +9,8 @@ int main() {
       if (f != NULL) {
 
         fscanf(f, "%d", &V);
-        fprintf(stdout, "il valore al interno del file è pari a %d", V);
-      
+        fprintf(stdout, "il valore al interno del file è pari a %d\n", V);
+
         fclose(f);
       }else{
         fprintf(stderr, "error 404 \n");
@@ -21,6 +21,13 @@ int main() {
 
       num = num+V;
       fprintf(stdout,"il nuovo valore è == %d\n", num);
+
+f = fopen("sum.dat", "w");
+
+     fprintf(f, "%d\n", num);
+
+fclose(f);
+
 
 
   return 0;
